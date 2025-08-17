0258d4f00c164ecf1cdc3200dc06ac8891052f53
TheQueenIsDead <30706552+TheQueenIsDead@users.noreply.github.com>
2025-08-17 21:27:01 +1200

    feat: initial replayer implementation

--- cmd/replay/main.go
+++ cmd/replay/main.go
@@ -1,12 +1,12 @@
 package main
 
 import (
-	"context"
 	"fmt"
-	"github.com/go-kit/log"
 	"github.com/philippseith/signalr"
-	"net"
+	"gophormula/pkg/replay"
+	"log"
 	"os"
+	"path/filepath"
 )
 
 type ReplayHub struct {
@@ -28,28 +28,25 @@ func (r ReplayHub) OnDisconnected(connectionID string) {
 }
 
 func main() {
-	ctx := context.Background()
 
-	hub := ReplayHub{}
-
-	// Typical server with log level debug to Stderr
-	server, err := signalr.NewServer(ctx,
-		signalr.SimpleHubFactory(hub),
-		signalr.Logger(log.NewLogfmtLogger(os.Stderr), true),
-	)
-	if err != nil {
-		panic(err)
+	if len(os.Args) < 2 {
+		fmt.Println("Usage: replay <dir>")
 	}
 
-	fmt.Println("Listening on localhost:6502")
+	dir := os.Args[1]
 
-	// Serving over TCP, accepting client who use MessagePack or JSON
-	addr, _ := net.ResolveTCPAddr("tcp", "localhost:6502")
-	listener, _ := net.ListenTCP("tcp", addr)
-	tcpConn, _ := listener.Accept()
-
-	err = server.Serve(signalr.NewNetConnection(ctx, tcpConn))
+	replayer := replay.New()
+	err := replayer.ParseGlob(filepath.Join(dir, "*"))
 	if err != nil {
-		panic(err)
+		log.Fatal(err)
+	}
+	ch := replayer.StartAndSubscribe()
+	defer replayer.Close()
+
+	for {
+		select {
+		case message := <-ch:
+			fmt.Println(message)
+		}
 	}
 }
---
+++ pkg/replay/replay.go
@@ -0,0 +1,113 @@
+package replay
+
+import (
+	"bufio"
+	"bytes"
+	"errors"
+	"io"
+	"log"
+	"os"
+	"path/filepath"
+	"strings"
+	"time"
+)
+
+type Replayer struct {
+	files       []os.File
+	subscribers []*chan any
+}
+
+func New() *Replayer {
+	return &Replayer{}
+}
+
+func (r *Replayer) ParseGlob(glob string) error {
+
+	matches, err := filepath.Glob(glob)
+	if err != nil {
+		log.Println("error matching glob: ", err)
+		return err
+	}
+	if len(matches) == 0 {
+		log.Println("no matching files")
+		return err
+	}
+
+	for _, match := range matches {
+		if strings.HasPrefix(match, ".") {
+			continue
+		}
+
+		log.Println("processing", filepath.Base(match))
+
+		file, err := os.Open(match)
+		if err != nil {
+			log.Fatal(err)
+		}
+		r.files = append(r.files, *file)
+		//defer file.Close()
+	}
+
+	return nil
+}
+
+func (r *Replayer) broadcast(message any) {
+	for _, subscriber := range r.subscribers {
+		ch := *subscriber
+		ch <- message
+	}
+}
+
+func (r *Replayer) Start() {
+	for _, file := range r.files {
+		scanner := bufio.NewScanner(&file)
+		for scanner.Scan() {
+			line := scanner.Text()
+			r.broadcast(line)
+			// FIXME: Need to remove this in place of logic that pauses at appropriate times to simulate replay
+			time.Sleep(1 * time.Millisecond)
+		}
+	}
+}
+
+func (r *Replayer) Subscribe() <-chan any {
+	ch := make(chan any, 64)
+	r.subscribers = append(r.subscribers, &ch)
+	return ch
+}
+
+func (r *Replayer) StartAndSubscribe() <-chan any {
+	go r.Start()
+	return r.Subscribe()
+}
+
+func (r *Replayer) Close() error {
+	var errs []error
+	for _, file := range r.files {
+		err := file.Close()
+		if err != nil {
+			errs = append(errs, err)
+			log.Printf("error closing file: %x\n", file.Name())
+		}
+	}
+	return errors.Join(errs...)
+}
+
+func lineCounter(r io.Reader) (int, error) {
+	buf := make([]byte, 32*1024)
+	count := 0
+	lineSep := []byte{'\n'}
+
+	for {
+		c, err := r.Read(buf)
+		count += bytes.Count(buf[:c], lineSep)
+
+		switch {
+		case err == io.EOF:
+			return count, nil
+
+		case err != nil:
+			return count, err
+		}
+	}
+}
