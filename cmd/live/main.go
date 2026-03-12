4c152482f0ff322752819943d5622985ef3c4bac
TheQueenIsDead <30706552+TheQueenIsDead@users.noreply.github.com>
2026-03-12 13:06:32 +1300

    chore: refactor live cmd string reprs into livetiming messages

--- cmd/live/main.go
+++ cmd/live/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"gophormula/pkg/livetiming"
 	"gophormula/pkg/signalr"
 	"log/slog"
@@ -31,43 +32,9 @@ func main() {
 	}
 
 	slog.Info("waiting for messages")
-	for {
-		select {
-		case msg := <-ch:
-			for _, data := range livetiming.ParseJSON(msg.Data()) {
-				switch v := data.(type) {
-				case *livetiming.Heartbeat:
-					slog.Debug("heartbeat", "utc", v.Utc)
-				case *livetiming.CarData:
-					slog.Debug("car data", "cars", len(v.Entries))
-				case *livetiming.PositionData:
-					slog.Debug("position data")
-				case *livetiming.SessionInfo:
-					slog.Info("session info", "meeting", v.Meeting.Name, "session", v.Name)
-				case *livetiming.TimingData:
-					slog.Debug("timing data", "drivers", len(v.Lines))
-				case *livetiming.TopThree:
-					slog.Debug("top three", "drivers", len(v.Lines))
-				case *livetiming.TimingStats:
-					slog.Debug("timing stats", "drivers", len(v.Lines))
-				case *livetiming.TimingAppData:
-					slog.Debug("timing app data", "drivers", len(v.Lines))
-				case *livetiming.WeatherData:
-					slog.Info("weather", "air", v.AirTemp, "track", v.TrackTemp)
-				case *livetiming.TrackStatus:
-					slog.Info("track status", "status", v.Status, "message", v.Message)
-				case *livetiming.DriverList:
-					slog.Info("driver list", "drivers", len(*v))
-				case *livetiming.RaceControlMessages:
-					slog.Info("race control", "messages", len(v.Messages))
-				case *livetiming.SessionData:
-					slog.Debug("session data", "points", len(v.Series))
-				case *livetiming.LapCount:
-					slog.Info("lap count", "current", v.CurrentLap, "total", v.TotalLaps)
-				default:
-					slog.Warn("unknown message type", "type", data)
-				}
-			}
+	for msg := range ch {
+		for _, data := range livetiming.ParseJSON(msg.Data()) {
+			slog.Info(fmt.Sprint(data))
 		}
 	}
 }
